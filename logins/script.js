0f4c756305cb5892a0d81c8b995e2e4a120c2881
LukaMoraes <170033178+LukaMoraes@users.noreply.github.com>
2024-06-10 17:15:07 -0300

    Cópia de um site que peguei do Canva. Até então indo ok. Hora de aprender de verdade javascript. HTML e CSS tá ok, parece.

---
+++ logins/script.js
@@ -0,0 +1,8 @@
+document.getElementById('loginForm').addEventListener('submit', 
+    function(event) {
+    event.preventDefault();
+})
+
+const usernameLogin = document.getElementById('username').value
+const passwordLogin = document.getElementById('password').value
+
